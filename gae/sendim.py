5f02db49055f44976a1dd075b024d0dfcb92d446
harryxu <harryzhxu@gmail.com>
2010-06-23 14:51:43 +0800

    init

---
+++ gae/sendim.py
@@ -0,0 +1,39 @@
+import os
+
+from google.appengine.api import xmpp
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
+
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        user_address = self.request.get('jid')
+
+        msg = "test message"
+
+        if not xmpp.get_presence(user_address):
+            xmpp.send_invite(user_address)
+        else:
+            status_code = xmpp.send_message(user_address, msg)
+            self.response.out.write(status_code)
+
+class InviteHandler(webapp.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'tpl/sendim/invite.html')
+        self.response.out.write(template.render(path, {}))
+
+    def post(self):
+        email = self.request.get('email')
+        xmpp.send_invite(email)
+        self.response.out.write('Invation has been sent to %s.' % email)
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/sendim', MainHandler),
+        ('/sendim/invite', InviteHandler),
+    ], debug=True)
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
